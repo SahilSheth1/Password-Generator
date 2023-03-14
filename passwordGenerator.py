d8d1736bdfc940ac9b68c162d664ab11dde4990c
LastBrainCells <sahilshethdevelopment@gmail.com>
2023-03-14 13:37:15 -0500

    Finished code for password generator

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -2,11 +2,16 @@ import random
 
 characters = "abcdefghijklmnopqrstuvwxyz1234567891234567890!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567891234567890!@#$%^&*()"
 
-while True:
-    passwordLength = int(input("What is the length of your desired password: "))
-    passwordCount = int(input("How many passwords would you like: "))
-    for x in range(0, passwordCount):
-        password = ""
-        for x in range(0, passwordLength):
-            passwordCharacter = random.choice(characters)
-            print(passwordCharacter)
+passwordLength = int(input("What is the length of your desired password: "))
+passwordCount = int(input("How many passwords would you like: "))
+
+print("Passwords: \n")
+
+for x in range(0, passwordCount):
+    password = ""
+    
+    for y in range(0, passwordLength):
+        passwordCharacter = random.choice(characters)
+        password = password + passwordCharacter
+        
+    print(password, "\n")
