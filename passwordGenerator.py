db94fb011ddeb1e7bbcea780707ea9ce0eed9ec6
LastBrainCells <sahilshethdevelopment@gmail.com>
2023-03-14 13:02:33 -0500

    Added for loop and tested prompts and variable values

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -5,4 +5,5 @@ characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12345678912345
 while True:
     passwordLength = int(input("What is the length of your desired password: "))
     passwordCount = int(input("How many passwords would you like: "))
-        
+    for x in range(0, passwordCount):
+        print(x)
